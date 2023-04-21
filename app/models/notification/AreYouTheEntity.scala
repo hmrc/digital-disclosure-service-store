a61c7d3ac522cd994e08a9c11166a124ccc71c2c
Mike Burns <13621631+burns238@users.noreply.github.com>
2023-04-21 10:07:44 +0100

    DDS2-2117 - update AreYouTheEntity, still allowing for bools in mongo

---
+++ app/models/notification/AreYouTheEntity.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.libs.json._
+
+sealed trait AreYouTheEntity
+
+object AreYouTheEntity {
+
+  case object YesIAm extends WithName("yes") with AreYouTheEntity
+  case object IAmAnAccountantOrTaxAgent extends WithName("accountant") with AreYouTheEntity
+  case object IAmAFriend extends WithName("friend") with AreYouTheEntity
+  case object VoluntaryOrganisation extends WithName("voluntaryOrganisation") with AreYouTheEntity
+  case object PowerOfAttorney extends WithName("powerOfAttorney") with AreYouTheEntity
+
+  implicit val reads: Reads[AreYouTheEntity] = Reads {
+    case JsString("yes") => JsSuccess(YesIAm)
+    case JsString("accountant") => JsSuccess(IAmAnAccountantOrTaxAgent)
+    case JsString("friend") => JsSuccess(IAmAFriend)
+    case JsString("voluntaryOrganisation") => JsSuccess(VoluntaryOrganisation)
+    case JsString("powerOfAttorney") => JsSuccess(PowerOfAttorney)
+    case JsString("true") => JsSuccess(YesIAm)
+    case JsString("false") => JsSuccess(IAmAnAccountantOrTaxAgent)
+    case _ => JsError("error.invalid")
+  }
+
+  implicit val writes = Writes[AreYouTheEntity](value => JsString(value.toString))
+
+  implicit val format: Format[AreYouTheEntity] = Format(reads, writes)
+
+}
---
+++ test/uk/gov/hmrc/digitaldisclosureservicestore/models/AreYouTheEntitySpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.{JsString, JsSuccess, Json}
+
+class AreYouTheEntitySpec extends AnyWordSpec with Matchers  {
+
+  "reads" should {
+    "convert JsString true to YesIAm" in {
+      val actual = JsString("true")
+      actual.validate[AreYouTheEntity] shouldEqual JsSuccess(AreYouTheEntity.YesIAm)
+    }
+
+    "convert JsString false to IAmAnAccountantOrTaxAgent" in {
+      val actual = JsString("false")
+      actual.validate[AreYouTheEntity] shouldEqual JsSuccess(AreYouTheEntity.IAmAnAccountantOrTaxAgent)
+    }
+
+    "convert JsString yes to YesIAm" in {
+      val actual = JsString("yes")
+      actual.validate[AreYouTheEntity] shouldEqual JsSuccess(AreYouTheEntity.YesIAm)
+    }
+
+    "convert JsString accountant to IAmAnAccountantOrTaxAgent" in {
+      val actual = JsString("accountant")
+      actual.validate[AreYouTheEntity] shouldEqual JsSuccess(AreYouTheEntity.IAmAnAccountantOrTaxAgent)
+    }
+
+    "convert JsString friend to IAmAFriend" in {
+      val actual = JsString("friend")
+      actual.validate[AreYouTheEntity] shouldEqual JsSuccess(AreYouTheEntity.IAmAFriend)
+    }
+
+    "convert JsString voluntaryOrganisation to VoluntaryOrganisation" in {
+      val actual = JsString("voluntaryOrganisation")
+      actual.validate[AreYouTheEntity] shouldEqual JsSuccess(AreYouTheEntity.VoluntaryOrganisation)
+    }
+
+    "convert JsString powerOfAttorney to PowerOfAttorney" in {
+      val actual = JsString("powerOfAttorney")
+      actual.validate[AreYouTheEntity] shouldEqual JsSuccess(AreYouTheEntity.PowerOfAttorney)
+    }
+
+  }
+
+  "writes" should {
+    "convert YesIAm to JsString yes" in {
+      val actual: AreYouTheEntity = AreYouTheEntity.YesIAm
+      Json.toJson(actual) shouldEqual JsString("yes")
+    }
+
+    "convert IAmAnAccountantOrTaxAgent to JsString accountant" in {
+      val actual: AreYouTheEntity = AreYouTheEntity.IAmAnAccountantOrTaxAgent
+      Json.toJson(actual) shouldEqual JsString("accountant")
+    }
+
+  }
+  
+}
